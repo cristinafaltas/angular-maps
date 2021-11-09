49ce47b8f50f0b6c9f7619d63da476b34b70765b
cristinafaltas <faltas.cristina@iisgalvanimi.edu.it>
2021-11-09 12:42:46 +0000

    quasi fine parte 2

--- server-pitone/main.py
+++ server-pitone/main.py
@@ -1,22 +1,33 @@
-#main.py
-# Import the Flask module that has been installed.
 from flask import Flask
-from flask import send_file
-
-# Creating a new "app" by using the Flask constructor. Passes __name__ as a parameter.
+from flask import jsonify
+from flask import request
+from flask_pymongo import PyMongo
+from flask_cors import CORS
+ 
 app = Flask(__name__)
-
+ 
+# Stringa di connessione al DB
+app.config["MONGO_URI"] = "mongodb+srv://cristina:9wwDgRvhmpEVdCjt@cluster0.7nx5c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" #Importante qui va specificato il nome del DB
+ 
+mongo = PyMongo(app)
+# Per rispondere alle chiamate cross origin
+CORS(app)
+ 
 # Annotation that allows the function to be hit at the specific URL.
-@app.route("/")
+@app.route("/pippo")
 # Generic Python functino that returns "Hello world!"
 def index():
     return "Hello world!"
-# Annotation that allows the function to be hit at the specific URL (/books).
-@app.route("/books")
-# Generic Python functino that returns books.json
-def books():
-    return send_file('books.json')
-    
+ 
+# Questa route effettua una find() su tutto il DB (si limita ai primi 100 risultati)
+@app.route('/addresses', methods=['GET'])
+def get_all_addresses():
+    mil4326WKT = mongo.db.MilWKT4326
+    output = []
+    for s in mil4326WKT.find().limit(100):
+        output.append(s['INDIRIZZO'])
+    return jsonify({'result': output})
+ 
 # Checks to see if the name of the package is the run as the main package.
 if __name__ == "__main__":
     # Runs the Flask application only if the main.py file is being run.

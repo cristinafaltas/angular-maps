2636710273b44c7b8d1756c41fa269451b5fff80
cristinafaltas <faltas.cristina@iisgalvanimi.edu.it>
2021-11-09 12:51:24 +0000

    quasi fine parte 2 corretto

--- server-pitone/main.py
+++ server-pitone/main.py
@@ -7,14 +7,14 @@ from flask_cors import CORS
 app = Flask(__name__)
  
 # Stringa di connessione al DB
-app.config["MONGO_URI"] = "mongodb+srv://cristina:9wwDgRvhmpEVdCjt@cluster0.7nx5c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" #Importante qui va specificato il nome del DB
+app.config["MONGO_URI"] = "mongodb+srv://cristina:9wwDgRvhmpEVdCjt@cluster0.7nx5c.mongodb.net/Relab?retryWrites=true&w=majority" #Importante qui va specificato il nome del DB
  
 mongo = PyMongo(app)
 # Per rispondere alle chiamate cross origin
 CORS(app)
  
 # Annotation that allows the function to be hit at the specific URL.
-@app.route("/pippo")
+@app.route("/")
 # Generic Python functino that returns "Hello world!"
 def index():
     return "Hello world!"
@@ -31,4 +31,4 @@ def get_all_addresses():
 # Checks to see if the name of the package is the run as the main package.
 if __name__ == "__main__":
     # Runs the Flask application only if the main.py file is being run.
-    app.run()
+    app.run(port=5001)

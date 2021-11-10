7959d94a5220761969517a17be6efc721e6676c9
cristinafaltas <faltas.cristina@iisgalvanimi.edu.it>
2021-11-10 12:58:56 +0000

    quasi quasi fine parte 2

--- server-pitone/main.py
+++ server-pitone/main.py
@@ -20,15 +20,24 @@ def index():
     return "Hello world!"
  
 # Questa route effettua una find() su tutto il DB (si limita ai primi 100 risultati)
-@app.route('/addresses', methods=['GET'])
+@app.route('/ciao', methods=['GET'])
 def get_all_addresses():
     mil4326WKT = mongo.db.MilWKT4326
     output = []
     for s in mil4326WKT.find().limit(100):
-        output.append(s['INDIRIZZO'])
+        output.append(s['INDIRIZZO'] + "|" + s["CI_VETTORE"])
     return jsonify({'result': output})
  
+@app.route('/ci_vettore', methods=['GET'])
+def get_vettore():
+    mil4326WKT = mongo.db.MilWKT4326
+    output = []
+    for s in mil4326WKT.find().limit(100):
+        output.append(s['CI_VETTORE'])
+    return jsonify({'result': output})
+    
 # Checks to see if the name of the package is the run as the main package.
 if __name__ == "__main__":
     # Runs the Flask application only if the main.py file is being run.
     app.run(port=5001)
+

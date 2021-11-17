9540baf9fcc5c7ee4277214da2b760b96b053d78
cristinafaltas <faltas.cristina@iisgalvanimi.edu.it>
2021-11-17 12:42:53 +0000

    fine relab parte 2

--- server-pitone/main.py
+++ server-pitone/main.py
@@ -37,16 +37,45 @@ def get_vettore():
     return jsonify({'result': output})
     
 @app.route('/ci_vettore/<foglio>', methods=['GET'])
-def get_vettore2(foglio):
+def get_vettore1(foglio):
     mil4326WKT = mongo.db.MilWKT4326
     output = []
     query = {
         "FOGLIO" : foglio
     }
-    for s in mil4326WKT.find(query).limit(100):
-        output.append(s['CI_VETTORE'])
-    return jsonify({'result': output})
+    for s in mil4326WKT.find(query):
+        output.append({
+            "INDIRIZZO":s['INDIRIZZO'],
+            "WGS84_X":s["WGS84_X"],
+            "WGS84_Y":s["WGS84_Y"],
+            "CLASSE_ENE":s["CLASSE_ENE"],
+            "EP_H_ND":s["EP_H_ND"],
+            "FOGLIO":s["FOGLIO"],
+            "CI_VETTORE":s['CI_VETTORE']
+        }
+        )
+    return jsonify(output) #Nota che abbiamo eliminato la chiave result perchè i dati sono già formattati
 
+@app.route('/ci_vettore/sezione/<sezione>', methods=['GET'])
+def get_vettore2(sezione):
+    mil4326WKT = mongo.db.MilWKT4326
+    output = []
+    query = {
+        "SEZ" : sezione
+    }
+    for s in mil4326WKT.find(query):
+        output.append({
+            "INDIRIZZO":s['INDIRIZZO'],
+            "WGS84_X":s["WGS84_X"],
+            "WGS84_Y":s["WGS84_Y"],
+            "CLASSE_ENE":s["CLASSE_ENE"],
+            "EP_H_ND":s["EP_H_ND"],
+            "FOGLIO":s["FOGLIO"],
+            "SEZ":s["SEZ"],
+            "CI_VETTORE":s['CI_VETTORE']
+        }
+        )
+    return jsonify(output)
 # Checks to see if the name of the package is the run as the main package.
 if __name__ == "__main__":
     # Runs the Flask application only if the main.py file is being run.
